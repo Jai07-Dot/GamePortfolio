c22b36d6f2497404118910081ccf8a7f3f76da17
Jai07-Dot <jairasettles8@gmail.com>
2026-07-22 17:47:56 -0500

    Add author metadata and system initialization
    Added author metadata and initialization function for serial output.

--- myblinks/myblinks.ino
+++ myblinks/myblinks.ino
@@ -1,13 +1,27 @@
 // The code that blinks my name in morse code.
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
 
+// ============================================================================
+// AUTHOR METADATA
+// ============================================================================
+const char* AUTHOR = "Jaira Settles";
+const char* VERSION = "1.0.0";
+void initSystem() {
+  Serial.begin(9600);
+  Serial.print("[");
+  Serial.print(AUTHOR);
+  Serial.print(" Core Engine v");
+  Serial.print(VERSION);
+  Serial.println("] Starting...");
+}
+
+void setup() {
+  initSystem();
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 // timing for the blinks
 int dotTime = 200;
   int dashTime = 600;
   int letterSpace = 800;
-
 void dot() {
 digitalWrite(LED_BUILTIN, HIGH);
 delay(dotTime);
@@ -15,7 +29,6 @@ digitalWrite(LED_BUILTIN, LOW);
 delay(dotTime);
  
    }
-
 void dash() {
  digitalWrite(LED_BUILTIN, HIGH);
 delay(dashTime);
@@ -23,23 +36,18 @@ digitalWrite(LED_BUILTIN, LOW);
 delay(dashTime);
  
    }
-
 void loop() {
 //J(.---)
 dot(); dash(); dash(); dash();
 delay(letterSpace);
-
 //A(.-)
 dot(); dash();
-
  //I(..)
  dot(); dot();
  delay(letterSpace);
-
  //R(.-.)
  dot(); dash(); dot();
  delay(letterSpace);
-
  //A(.-)
  dot(); dash();
  delay(2000); //pause before repeating
